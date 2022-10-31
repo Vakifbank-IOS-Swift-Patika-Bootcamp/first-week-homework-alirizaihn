c0e2176b4e41e8f654bceac406a3d6f816594a0f
Ali Rıza <aliriza.ihn@gmail.com>
2022-10-31 14:16:42 +0300

    -Question 3-4 added

--- First-Week-Homework.playground/Contents.swift
+++ First-Week-Homework.playground/Contents.swift
@@ -15,7 +15,6 @@ func checkPalindrome(word:String){
         }
     }
 }
-checkPalindrome(word: "Veliilev")
 
 //================== 2. Question =================
 
@@ -26,5 +25,76 @@ func calculateItemCount(items: [String]){
     }
     print(countOfItems)
 }
+
+
+//=============== 3 - 4 Question ====================
+
+func createTree(rowCount:Int){
+    var space = rowCount
+    for i in 1...rowCount {
+//       ================ 4 Question ====================
+        for k in 1...space{
+            print(" " ,terminator: "")
+        }
+//      =================================================
+        for p in 1...i {
+            print("*",terminator: " ")
+        }
+        space -= 1
+        print("\r")
+    }
+}
+
+// ============== 5.Question =================
+
+// 5.3 Question
+
+func isPrimeNumber(candidateNumber:Int) -> Bool{
+    var result = true
+    if candidateNumber <= 1{
+        return false
+    }
+    if candidateNumber != 2 {
+        for i in 2..<candidateNumber{
+            if candidateNumber % i == 0 {
+                return false
+            }
+        }
+    }
+    return result
+}
+
+func findLargestPrimeFactor(numberExample:Int){
+    var resultNumber = numberExample
+    var divisorsSet: Set<Int> = []
+    while(resultNumber > 1){
+        for i in 2...resultNumber{
+            if resultNumber % i == 0{
+                resultNumber = resultNumber / i
+                if(isPrimeNumber(candidateNumber: i)){
+                    divisorsSet.insert(i)
+                }
+                break
+            }
+            else {
+                continue
+            }
+        }
+    }
+    var divisorsNumberArray = divisorsSet.sorted()
+    let largetFactor = divisorsNumberArray[divisorsNumberArray.count-1]
+    print("Largest prime factor is \(largetFactor)")
+    
+}
+/*Qestion 1 => */
+checkPalindrome(word: "Veliilev")
+/*Qestion 2 => */
 calculateItemCount(items: ["1","b","g","1","b","g","v","s"])
+/*Qestion 3 - 4 => */
+createTree(rowCount: 4)
+/*Qestion 5.1 => */
+/*Qestion 5.2 => */
+/*Qestion 5.3 => */
+findLargestPrimeFactor(numberExample: 600851475143)
+
 
