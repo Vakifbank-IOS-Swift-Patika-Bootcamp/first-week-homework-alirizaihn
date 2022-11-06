7aa2b42fe42d62a748ca1a98edfde34f128bfb41
Ali Rıza <aliriza.ihn@gmail.com>
2022-11-06 17:25:10 +0300

    -Final

--- First-Week-Homework.playground/Contents.swift
+++ First-Week-Homework.playground/Contents.swift
@@ -1,44 +1,46 @@
 import Foundation
+
 //================== 1. Question =================
-func checkPalindrome(word:String){
-    let wordLength = word.count
+
+func checkPalindrome(word: String) {
+    var isPalindrome = false
     let lowerCaseWord = word.lowercased()
     let charSet = Array(lowerCaseWord)
-    for i in 0..<wordLength{
-        if charSet[i] != charSet[wordLength - 1 - i]{
-            print("\(word) isn't palindrome")
+    let filteredCharArray = charSet.filter { (characterExample) -> Bool in
+        characterExample.isLetter || characterExample.isNumber
+    }
+    let wordLength = filteredCharArray.count
+    for i in 0..<wordLength {
+        if filteredCharArray[i] != filteredCharArray[wordLength - 1 - i] {
+            isPalindrome = false
             break
-        }
-        else {
-            print("\(word) is palindrome")
-            break
+        } else {
+            isPalindrome = true
         }
     }
+    print(isPalindrome ? ("\(word) is palindrome") : "\(word) isn't palindrome" )
 }
 
 //================== 2. Question =================
 
-func calculateItemCount(items: [String]){
-    var countOfItems: [String:Int] = [:]
-    for item in items{
-        countOfItems[item] = (countOfItems[item] ?? 0) + 1
+func calculateItemCount<T>(items: [T]) {
+    var countOfItems: [String: Int] = [:]
+    for item in items {
+        countOfItems[String(describing: item)] = (countOfItems[String(describing: item)] ?? 0) + 1
     }
     print(countOfItems)
 }
 
-
 //=============== 3 - 4 Question ====================
 
-func createTree(rowCount:Int){
+func createTree(rowCount: Int) {
     var space = rowCount
     for i in 1...rowCount {
-//       ================ 4 Question ====================
-        for k in 1...space{
-            print(" " ,terminator: "")
+        for _ in 1...space {
+            print(" ", terminator: "")
         }
-//      =================================================
-        for p in 1...i {
-            print("*",terminator: " ")
+        for _ in 1...i {
+            print("*", terminator: " ")
         }
         space -= 1
         print("\r")
@@ -47,54 +49,55 @@ func createTree(rowCount:Int){
 
 // ============== 5.Question =================
 
-// 5.3 Question
-
-func isPrimeNumber(candidateNumber:Int) -> Bool{
-    var result = true
-    if candidateNumber <= 1{
-        return false
-    }
-    if candidateNumber != 2 {
-        for i in 2..<candidateNumber{
-            if candidateNumber % i == 0 {
-                return false
-            }
-        }
-    }
-    return result
+// 5.1 Question
+func sumOfAllMultiples(numberExample: Int) {
+    let number = numberExample - 1
+    let sumOfThree = 3 * ((number / 3) * ((number / 3) + 1)) / 2
+    let sumOfFive = 5 * ((number / 5) * ((number / 5) + 1)) / 2
+    let sumOfFifteen = 15 * ((number / 15) * ((number / 15) + 1)) / 2
+    print(sumOfThree + sumOfFive - sumOfFifteen)
 }
 
-func findLargestPrimeFactor(numberExample:Int){
-    var resultNumber = numberExample
-    var divisorsSet: Set<Int> = []
-    while(resultNumber > 1){
-        for i in 2...resultNumber{
-            if resultNumber % i == 0{
-                resultNumber = resultNumber / i
-                if(isPrimeNumber(candidateNumber: i)){
-                    divisorsSet.insert(i)
-                }
-                break
-            }
-            else {
-                continue
-            }
+// 5.2 Question
+func sumOfEvenFibonacciNumbers(numberExample: Int) {
+    var sumOfNumbers = 0
+    var firstNumber = 1
+    var secondNumber = 2
+    while secondNumber <= numberExample {
+        if secondNumber % 2 == 0 {
+            sumOfNumbers += secondNumber
         }
+        let newNumber = firstNumber + secondNumber
+        firstNumber = secondNumber
+        secondNumber = newNumber
     }
-    var divisorsNumberArray = divisorsSet.sorted()
-    let largetFactor = divisorsNumberArray[divisorsNumberArray.count-1]
-    print("Largest prime factor is \(largetFactor)")
+    print(sumOfNumbers)
     
 }
-/*Qestion 1 => */
-checkPalindrome(word: "Veliilev")
-/*Qestion 2 => */
-calculateItemCount(items: ["1","b","g","1","b","g","v","s"])
-/*Qestion 3 - 4 => */
-createTree(rowCount: 4)
-/*Qestion 5.1 => */
-/*Qestion 5.2 => */
-/*Qestion 5.3 => */
-findLargestPrimeFactor(numberExample: 600851475143)
-
+// 5.3 Question
+func findLargestPrimeFactor(numberExample: Int) {
+    var resultNumber = numberExample
+    for i in 2...resultNumber {
+        if (i * i) <= resultNumber {
+            while (resultNumber % i == 0) && (resultNumber != i) {
+                resultNumber /= i
+            }
+        } else {
+            break
+        }
+    }
+    print("Largest prime factor is \(resultNumber)")
+}
+//Qestion 1 =>
+checkPalindrome(word: "İmrensem adama madam esner mİ?")
+//Qestion 2 => */
+calculateItemCount(items: [12.4, false, 1, "false"])
+//Qestion 3 - 4 =>
+createTree(rowCount: 8)
+//Qestion 5.1 =>
+sumOfAllMultiples(numberExample: 1000)
+//Qestion 5.2 =>
+sumOfEvenFibonacciNumbers(numberExample: 4_000_000)
+//Qestion 5.3 =>
+findLargestPrimeFactor(numberExample: 13195)
 
